5e0c886ccf82a2a56c384fa053f66914d59a817c
Anirudh Bellamkonda <hyanirudh@gmail.com>
2021-07-14 23:38:50 -0500

    - Project init

---
+++ src/main/java/com/anirudhb2b/mail/AbEcomMailApplication.java
@@ -0,0 +1,13 @@
+package com.anirudhb2b.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbEcomMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbEcomMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anirudhb2b/mail/AbEcomMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anirudhb2b.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbEcomMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
